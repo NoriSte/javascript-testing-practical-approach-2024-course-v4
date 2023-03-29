3b6004f89fd021c06adcb255f9d3ecb733504008
Stefano Magni <nori.ste.magni@gmail.com>
2023-03-29 12:26:07 +0200

    Add a cy.session-based exercise

--- cypress/e2e/exercises/1-e2e-tests/13-sharing-session.e2e.spec.js
+++ cypress/e2e/exercises/1-e2e-tests/13-sharing-session.e2e.spec.js
@@ -2,24 +2,26 @@
 
 /**
  * Main goals
- * - Store the jwt token of the first test and reuse it in the next tests
+ * - Transform the previous manually-created state-sharing test into a cy.session-based test
  *
  * What to learn
- * - Again: speeding up the test
- * - Tests must work independently from other tests
- *
- * Testing rules
- * - Deterministic tests means also that you should not rely on their execution order
+ * - To leverage the dedicated cy.session command
  */
 
-let previousJwt
-let previousCredentials = {}
-
-Cypress.Commands.add('register', { prevSubject: 'optional' }, function (_subject) {
+Cypress.Commands.add('register', function (username, email, password) {
   // ...
 })
 
 context('The New Post page', () => {
+  const random = Math.round(Math.random() * 1000000)
+  const credentials = {
+    username: `foo${random}`,
+    email: `foo${random}@bar.com`,
+    password: 'bazbazbaz',
+  }
+
+  before(() => {})
+
   beforeEach(() => {
     // adapt the viewport, allows the instructor to have more vertical windows when sharing the screen
     cy.viewport(600, 900)
@@ -27,14 +29,14 @@ context('The New Post page', () => {
 
   // Skipped until implemented
   it.skip('Should get the user registered', () => {
-    cy.register()
+    cy.register(credentials.username, credentials.email, credentials.password)
     cy.visit('/editor')
     cy.findByText('New Post').should('be.visible')
   })
 
   // Skipped until implemented
   it.skip('Should get the user registered (leveraging the previous user)', () => {
-    cy.register()
+    cy.register(credentials.username, credentials.email, credentials.password)
     cy.visit('/editor')
     cy.findByText('New Post').should('be.visible')
   })
---
+++ cypress/e2e/exercises/1-e2e-tests/13-sharing-session.solution.e2e.spec.js
@@ -0,0 +1,62 @@
+/// <reference types="Cypress" />
+
+/**
+ * Main goals
+ * - Transform the previous manually-created state-sharing test into a cy.session-based test
+ *
+ * What to learn
+ * - To leverage the dedicated cy.session command
+ */
+
+Cypress.Commands.add('register', function (username, email, password) {
+  cy.session(
+    [username, email, password],
+    () => {
+      // ------------------------------
+      // the registration flow
+      cy.intercept('POST', '**/api.realworld.io/api/users').as('signup-request')
+      cy.visit('/register')
+      cy.window().its('appActions').invoke('signup', { username, email, password })
+      cy.wait('@signup-request').then(_interception => {
+        // ... all the payload assertions are skipped for brevity...
+      })
+      cy.findByText('New Post').should('be.visible')
+    },
+    {
+      validate() {
+        cy.visit('/')
+
+        // use the 'New Post' string to detect if the user is authenticated or not
+        cy.findByText('New Post').should('be.visible')
+      },
+    },
+  )
+})
+
+context('The New Post page', () => {
+  const random = Math.round(Math.random() * 1000000)
+  const credentials = {
+    username: `foo${random}`,
+    email: `foo${random}@bar.com`,
+    password: 'bazbazbaz',
+  }
+
+  before(() => {})
+
+  beforeEach(() => {
+    // adapt the viewport, allows the instructor to have more vertical windows when sharing the screen
+    cy.viewport(600, 900)
+  })
+
+  it('Should get the user registered', () => {
+    cy.register(credentials.username, credentials.email, credentials.password)
+    cy.visit('/editor')
+    cy.findByText('New Post').should('be.visible')
+  })
+
+  it('Should get the user registered (leveraging the previous user)', () => {
+    cy.register(credentials.username, credentials.email, credentials.password)
+    cy.visit('/editor')
+    cy.findByText('New Post').should('be.visible')
+  })
+})
