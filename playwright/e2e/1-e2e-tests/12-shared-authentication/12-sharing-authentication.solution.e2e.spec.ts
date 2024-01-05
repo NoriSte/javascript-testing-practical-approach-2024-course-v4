82507b8dc3a7f7b08ea3e857b7ffefe51de715b1
Stefano Magni <nori.ste.magni@gmail.com>
2024-01-05 10:53:31 +0100

    Implement e2e/12 with authentication

---
+++ playwright/e2e/1-e2e-tests/12-shared-authentication/12-sharing-authentication.solution.e2e.spec.ts
@@ -0,0 +1,25 @@
+import { test } from "@playwright/test";
+
+/**
+ * Main goals
+ * - Store the jwt token of the first test and reuse it in the next tests
+ *
+ * What to learn
+ * - Again: speeding up the test
+ * - Tests must work independently from other tests
+ *
+ * Testing rules
+ * - Deterministic tests means also that you should not rely on their execution order
+ */
+
+test.use({
+  // adapt the viewport, allows the instructor to have more vertical windows when sharing the screen
+  viewport: { width: 600, height: 900 },
+});
+
+test.describe("The New Post page", () => {
+  test("Should get the user registered", async ({ page }) => {
+    await page.goto("/#/editor");
+    await page.getByRole("button", { name: "New Article" }).isVisible();
+  });
+});
